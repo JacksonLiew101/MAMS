70b1c2c181f61db0448cae34c7ad29882954d692
Shaun Liew Xin Hong <shaunliew20@gmail.com>
2022-01-06 17:33:22 +0800

    add login page and exit button

--- src/main/java/com/example/mams/HelloApplication.java
+++ src/main/java/com/example/mams/HelloApplication.java
@@ -1,18 +1,40 @@
 package com.example.mams;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
-    @Override
+    private double xOffset = 0;
+    private double yOffset = 0;
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login.fxml")));
+        Scene scene = new Scene(root);
+        stage.initStyle(StageStyle.UNDECORATED);
+        root.setOnMousePressed(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event){
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent> (){
+            @Override
+            public void handle(MouseEvent event)
+            {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+
+        });
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/mams/Login.java
@@ -0,0 +1,11 @@
+package com.example.mams;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+public class Login {
+    @FXML
+    private void exit(ActionEvent event){
+        System.exit(0);
+    }
+
+}
