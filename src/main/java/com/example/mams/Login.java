78fd2115e89db7611899fe8e49309bcdf962490d
Shaun Liew Xin Hong <shaunliew20@gmail.com>
2022-01-06 17:52:57 +0800

    add Hand cursor with enter the buttons and rename the helloApplication to MAMS

--- src/main/java/com/example/mams/Login.java
+++ src/main/java/com/example/mams/Login.java
@@ -1,11 +1,27 @@
 package com.example.mams;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-public class Login {
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Login implements Initializable {
+    @FXML
+    private Button button , button2;
+
     @FXML
     private void exit(ActionEvent event){
         System.exit(0);
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        button.setCursor(Cursor.HAND);
+        button2.setCursor(Cursor.HAND);
+    }
 }
--- src/main/java/com/example/mams/MAMS.java
+++ src/main/java/com/example/mams/MAMS.java
@@ -1,39 +1,29 @@
 package com.example.mams;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.util.Objects;
 
-public class HelloApplication extends Application {
+public class MAMS extends Application {
     private double xOffset = 0;
     private double yOffset = 0;
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login.fxml")));
         Scene scene = new Scene(root);
         stage.initStyle(StageStyle.UNDECORATED);
-        root.setOnMousePressed(new EventHandler<MouseEvent>(){
-            @Override
-            public void handle(MouseEvent event){
-                xOffset = event.getSceneX();
-                yOffset = event.getSceneY();
-            }
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
         });
-        root.setOnMouseDragged(new EventHandler<MouseEvent> (){
-            @Override
-            public void handle(MouseEvent event)
-            {
-                stage.setX(event.getScreenX() - xOffset);
-                stage.setY(event.getScreenY() - yOffset);
-            }
-
+        root.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - xOffset);
+            stage.setY(event.getScreenY() - yOffset);
         });
         stage.setScene(scene);
         stage.show();
